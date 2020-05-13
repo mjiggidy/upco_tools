5a5ebab7bcd7a7090c65ece6561e4d8f559bf63f
mjordan <michael@glowingpixel.com>
2020-05-13 03:58:48 -0700

    Initial separation of ltfs and shot libraries

--- upco_tools/upco_shot.py
+++ upco_tools/upco_shot.py
@@ -1,80 +1,47 @@
+import pathlib, enum
+import upco_timecode
+
 class Shot:
-	class Type(enum.Enum):
-		DIR, FILE = ("Directory", "File")
+
+	class Source(enum.Enum):
+		TAPE, FILE = ("Tape","File")
+
+	SPECIAL_COLUMNS = ("Tape","Source File Name","Start","End","Duration")
 		
-	def __init__(self, shot, fromlist="Custom", alts=None):
+	def __init__(self, shot, tc_start, tc_duration=None, tc_end=None, metadata=None, source=Source.TAPE, frm_rate=24000/1001):
 		
 		self.shot = shot
-		self.alts = alts
-		self.size = None
-		self.tape = None
-		self.from_list  = fromlist
+		self.frm_rate = frm_rate
+		self.source = self.__class__.Source(source)
+		self.metadata = {}
 		
+		self.set_tc_start(tc_start)
 
-	def getSize(self):
-		return sum([x.get("size",0) for x in self.filelist])
+		# Duration takes presidence over end TC if both are supplied
+		self.set_tc_duration(tc_duration) if tc_duration else self.set_tc_end(tc_end)
+
+		if metadata: self.add_metadata(metadata)
+
+
+	def set_tc_start(self, tc_start):
+		self.tc_start = upco_timecode.Timecode(tc_start, self.frm_rate)
 	
-	def getStartblock(self):
-		if len(self.filelist):
-			return min([x.get("startblock",0) for x in self.filelist])
+	def set_tc_duration(self, tc_duration):
+		self.tc_duration = upco_timecode.Timecode(tc_duration)
+	
+	def set_tc_end(self, tc_end):
+		tc_end = upco_timecode.Timecode(tc_end, self.frm_rate)
+		if self.tc_start < tc_end:
+			self.set_tc_duration(tc_end - self.tc_start)
 		else:
-			return 0
+			raise ValueError(f"TC End {tc_end} must not preceed TC Start {self.tc_start}")
 
-	def setPath(self, *args, **kwargs):
-
-		try: self.basepath = pathlib.Path(kwargs.get("basepath", '/'))
-		except Exception as e: raise Exception("Invalid path for shot {}: {}".format(kwargs.get("basepath",''), str(e)))
-		
-		try: self.tape = kwargs.get("tape")
-		except Exception as e: raise Exception("No source schema specified for shot")
-		
-		self.shot = kwargs.get("shot", self.shot)
-		self.type = kwargs.get("type", self.__class__.Type.DIR)
-		self.filelist = kwargs.get("filelist",[])
-		
-		
-		self.startblock = self.getStartblock()
-
-
-	def __eq__(self, cmp):
-		try: return str(cmp) == self.shot and cmp.getStartblock() == self.getStartblock()
-		except Exception as e: return False
+	def add_metadata(self, metadata):	
+		# Remove special columns
+		self.metadata.update({key:val for key, val in metadata.items() if key not in self.__class__.SPECIAL_COLUMNS})
 	
-	def __str__(self):
-		return self.shot
-	
-	def getShot(self):
-		return self.shot if self.shot else "Unknown Shot"
-
-	def getTape(self):
-		return self.tape if self.tape else "Unknown"
-	
-	def getMediaType(self):
-		#if self.type == self.__class__.Type.FILE:
-		#	if self.path and self.path.suffix: return self.path.suffix
-		#	else: return "Unknown (1)"
-		
-		#elif self.type == self.__class__.Type.DIR:
-		if len(self.filelist):
-			ext = ', '.join(set(x.get("path").suffix.lower() for x in self.filelist))
-			if len(ext): return ext
-			else: return "Unknown (2)"
+	def remove_metadata(self, metadata):
+		if type(metadata) is dict:
+			{self.metadata.pop(key) for key, val in metadata.items() if self.metadata.get(key) == val}
 		else:
-			return "Unknown (3)"
-	
-	# Alias
-	def getFormattedSize(self):
-		return self.getSizeAsString()
-	
-	def getSizeAsString(self):
-
-		size = self.getSize()
-
-		if size is None:
-			return "Unknown"
-		elif size < (1024*1024):
-			return "{:.2f} KB".format(size/(1024))
-		elif size < (1024*1024*1024):
-			return "{:.2f} MB".format(size/(1024*1024))
-		else:
-			return "{:.2f} GB".format(size/(1024*1024*1024))
+			self.metadata.pop(metadata, None)
