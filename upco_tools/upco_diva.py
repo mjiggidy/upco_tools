e358e43edce781b78777ef371df606efbdc1e508
Michael Jordan <michael.jordan@nbcuni.com>
2020-05-19 19:17:03 -0700

    Fleshing out restoreObject()

--- upco_tools/upco_diva.py
+++ upco_tools/upco_diva.py
@@ -1,12 +1,13 @@
 import subprocess, pathlib, enum
 
 class DivaCodes(enum.IntEnum):
-	OK = 0
-	MANAGER_NOT_FOUND = 1003
-	ALREADY_CONNECTED = 1006
-	OBJECT_NOT_FOUND  = 1009
-	DESTINATION_NOT_FOUND = 1018
-	LISTENER_NOT_FOUND = 4294967295 # 32-bit unsigned int max value
+	OK = 0								# Success
+	MANAGER_NOT_FOUND 		= 1003		# No manager at provided IP/Port
+	ALREADY_CONNECTED		= 1006		# Listener is already connected
+	INVALID_PARAMETER		= 1008		# Example: Invalid character in object name
+	OBJECT_NOT_FOUND		= 1009		# Object not found in given category (could also mean invalid category)
+	DESTINATION_NOT_FOUND	= 1018		# Invalid src/destination
+	LISTENER_NOT_FOUND		= 4294967295 #32-bit unsigned int max value, probably meant to be -1
 
 class Diva:
 
@@ -14,6 +15,7 @@ class Diva:
 		
 		self.man_ip = manager_ip
 		self.man_port = manager_port
+		self.last_request_id = None
 
 		if not pathlib.Path(divascript_path).is_file():
 			raise RuntimeError(f"Cannot find divascript at {divascript_path}")
@@ -26,8 +28,8 @@ class Diva:
 		
 		diva_client = subprocess.run([
 			str(self.divascript_exec), "connect",
-			"-mi", manager_ip,
-			"-mp", manager_port],
+			"-mi", str(manager_ip),
+			"-mp", str(manager_port)],
 			text = True,
 			capture_output = True
 		)
@@ -76,13 +78,29 @@ class Diva:
 			capture_output = True
 		)
 
-		print("In restoreObject()")
-		print(f"client.returncode: {diva_client.returncode}")
-		print(f"client.stdout: {diva_client.stdout}")
-		print(f"client.stderr: {diva_client.stderr}")
+		# Evaluate return code
+		if diva_client.returncode == DivaCodes.OK:
+			# If all is good, set last_request_id and return job ID
+			if diva_client.stdout.strip().isnumeric():
+				self.last_request_id = int(diva_client.stdout)
+				return self.last_request_id
+			else:
+				raise RuntimeWarning(f"Diva returned OK, but no request ID in response: {diva_client.stdout}")
+		
+		# If Object not found in Diva category
+		elif diva_client.returncode == DivaCodes.OBJECT_NOT_FOUND:
+			raise FileNotFoundError(f"Object {object_name} not found in category {category}")
 
-		#{print(f"server.stdout: {str(x)}") for x in self.diva_server.stdout.readlines()}
-		#{print(f"server.stderr: {str(x)}") for x in self.diva_server.stderr.readlines()}
+		# If destination is invalid
+		elif diva_client.returncode == DivaCodes.DESTINATION_NOT_FOUND:
+			raise NotADirectoryError(f"{destination} is not a valid restore destination")
+
+		# Catchall errors.  Mainly for debugging
+		elif diva_client.returncode in (code for code in DivaCodes):
+			raise RuntimeError(f"Error code {DivaCodes(diva_client.returncode).name}: {diva_client.stdout.strip()}")
+		
+		else:
+			raise RuntimeError(f"Unknown error code {diva_client.returncode}: {diva_client.stdout.strip()}")
 
 
 		
