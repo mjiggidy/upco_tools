dd2168e34b39b52d32d5e64d1bbb5e265623ef70
mjordan <michael@glowingpixel.com>
2020-07-12 23:47:48 -0700

    Initial work on CSV import

--- upco_tools/upco_shot.py
+++ upco_tools/upco_shot.py
@@ -1,10 +1,11 @@
-import pathlib, enum, re
+import pathlib, enum, re, csv
 from io import StringIO
 from . import upco_timecode
 
 class Shotlist:
+	"""Maintain a list of shots with support for common exchange formats"""
 
-	class __AleParseModes(enum.Enum):
+	class _AleParseModes(enum.Enum):
 		START = enum.auto()
 		HEADING = enum.auto()
 		COLUMN = enum.auto()
@@ -35,7 +36,7 @@ class Shotlist:
 		with path_input.open('r') as ale_input:
 
 			parsed_columns = []
-			parse_mode = cls.__AleParseModes.START
+			parse_mode = cls._AleParseModes.START
 			
 
 			# Spin through ALE file line by line and parse ALE
@@ -48,7 +49,7 @@ class Shotlist:
 				line_num += 1
 
 				# Parse Data block (main thing) =====
-				if parse_mode == cls.__AleParseModes.DATA:
+				if parse_mode == cls._AleParseModes.DATA:
 					
 					shot_data = line_data.split('\t')
 					
@@ -82,32 +83,34 @@ class Shotlist:
 							raise ValueError(f"Invalid timecode for shot on line {line_num} ({e})")
 						
 					# Add shot to shotlist
-					shotlist.addShot(Shot(
+					masterclip = Shot(
 						shot_name,
 						tc_start    = metadata.get("Start"),
 						tc_duration = metadata.get("Duration"),
 						tc_end      = metadata.get("End"),
-						metadata    = metadata,
+						#metadata    = metadata,
 						media       = shot_type,
-						source      = path_input,
+						#source      = path_input,
 						frm_rate    = ale_heading.get("FPS")
-					))
+					)
+					masterclip.addMetadata(metadata)
+					shotlist.addShot(masterclip)
 
 				# Parse Heading block =====
-				elif parse_mode == cls.__AleParseModes.HEADING:
+				elif parse_mode == cls._AleParseModes.HEADING:
 					
 					if line_data.lower() == "column":
-						parse_mode = cls.__AleParseModes.COLUMN
+						parse_mode = cls._AleParseModes.COLUMN
 						continue
 					
 					header = line_data.split('\t')
 					ale_heading.update({header[0]:header[1]})
 				
 				# Parse Column names ======
-				elif parse_mode == cls.__AleParseModes.COLUMN:
+				elif parse_mode == cls._AleParseModes.COLUMN:
 					
 					if line_data.lower() == "data":
-						parse_mode = cls.__AleParseModes.DATA
+						parse_mode = cls._AleParseModes.DATA
 						continue
 
 					elif len(parsed_columns):
@@ -122,9 +125,9 @@ class Shotlist:
 							raise SyntaxError(f"Found duplicate column names on line {line_num}:\n{','.join(dupes)}")
 
 				# File parsing starts here =====
-				elif parse_mode == cls.__AleParseModes.START:
+				elif parse_mode == cls._AleParseModes.START:
 					if line_data.lower() == "heading":
-						parse_mode = cls.__AleParseModes.HEADING
+						parse_mode = cls._AleParseModes.HEADING
 						continue
 
 					raise SyntaxError(f"Unexpected data before Heading on line {line_num}:\n{line_data}")
@@ -135,6 +138,34 @@ class Shotlist:
 		
 		return shotlist
 
+	@classmethod
+	def fromCSV(cls, path_input):
+
+		shotlist = cls()
+
+		path_input = pathlib.Path(path_input)
+		if not path_input.is_file(): raise FileNotFoundError(f"{path_input} is not found")
+
+		required_rows = ("Reel Name","Frame Rate","Start TC","Duration TC")
+
+		# TODO: Verify file encoding before parsing
+		with path_input.open('r', encoding="utf-16") as file_csv:
+			for num_row, row in enumerate(csv.DictReader(file_csv)):
+				if not all(required_rows):
+					raise ValueError(f"Missing column data for {', '.join(x for x in required_rows if row.get(x) is None)} on line {num_row+2}")
+				
+				shot = Shot(
+					shot = row.get("Reel Name"),
+					tc_start = row.get("Start TC"),
+					tc_duration = row.get("Duration TC"),
+					frm_rate = row.get("Frame Rate")
+				)
+
+				shot.addMetadata({x:row.get(x) for x in row.keys() if x not in required_rows})
+
+				shotlist.addShot(shot)
+		
+		return shotlist
 	
 	def __init__(self, shotlist=None):
 		"""
@@ -171,7 +202,7 @@ class Shotlist:
 		return self.shots	
 
 
-	def __buildAle(self, stream_output, preserveEmptyColumns=False, omitColumns=None, heading={"FIELD_DELIM":"TABS","VIDEO_FORMAT":1080,"FPS":23.98}, sourcecol="Tape"):
+	def _buildAle(self, stream_output, preserveEmptyColumns=False, omitColumns=None, heading={"FIELD_DELIM":"TABS","VIDEO_FORMAT":1080,"FPS":23.98}, sourcecol="Tape"):
 		"""
 		Private method to write formatted ALE to output stream.
 
@@ -233,7 +264,7 @@ class Shotlist:
 		"""
 
 		string_output = StringIO()
-		self.__buildAle(string_output, preserveEmptyColumns, omitColumns)
+		self._buildAle(string_output, preserveEmptyColumns, omitColumns)
 		return string_output.getvalue()
 		
 	def writeAle(self, path_output, preserveEmptyColumns=False, omitColumns=None):
@@ -257,11 +288,12 @@ class Shotlist:
 		path_output = pathlib.Path(path_output)
 		
 		with path_output.open('w') as file_output:
-			self.__buildAle(file_output, preserveEmptyColumns, omitColumns)
+			self._buildAle(file_output, preserveEmptyColumns, omitColumns)
 
 		return path_output
 
 class Shot:
+	"""Defines a shot"""
 
 	class MediaType(enum.Enum):
 		"""Indicates legacy Avid Tape-based workflow vs File-based with Source File Name"""
@@ -271,6 +303,7 @@ class Shot:
 	SPECIAL_COLUMNS = ("Tape","Source File Name","Start","End","Duration")
 
 	# Common OCN naming conventions
+	# TODO: May want to expand this into a dictionary eg. {Camera.ARRI, matchPattern} so a Shot can have a cameraType property
 	NAMING_PATTERNS = (re.compile(x, re.I) for x in (
 		r"(?P<camroll>[a-z][0-9]{3})(?P<clipindex>c[0-9]{3})_(?P<year>[0-9]{2})(?P<month>[0-9]{2})(?P<day>[0-9]{2})_(?P<camindex>[a-z])(?P<camid>[a-z0-9]{3})",	# Arri
 		r"(?P<camroll>[a-z][0-9]{3})_(?P<clipindex>[c,l,r][0-9]{3})_(?P<month>[0-9]{2})(?P<day>[0-9]{2})(?P<camid>[a-z0-9]{2})",								# Redcode/Venice
@@ -287,22 +320,25 @@ class Shot:
 		r"(?P<labroll>LR\d{8})"													# 35mm Labroll (Efilm)
 	))
 		
-	def __init__(self, shot, tc_start, tc_duration=None, tc_end=None, metadata=None, media=MediaType.TAPE, source=None, frm_rate=24000/1001):
+	def __init__(self, shot, tc_start, tc_duration=None, tc_end=None, media=MediaType.TAPE, frm_rate=24000/1001):
 		
-		self.shot       = shot
-		self.frm_rate   = frm_rate
-		self.media_type = self.__class__.MediaType(media)
-		self.source     = source
-		self.metadata   = {}
-		self.tc_start   = tc_start
+		self.shot       = str(shot)							# Shot name (ex A001C003_200711_R1CB)
+		self.frm_rate   = float(frm_rate)					# Video frame rate
+		self.media_type = self.__class__.MediaType(media)	# Avid Tape or Source File Name column.  May need some rethinking
+		self.metadata   = {}								# Non-critical metadata (Processed below)
+		self.tc_start   = tc_start							# Timecode start (accompanied by Timecode duration/end below)
+		
+		self._instances  = set()							# Media instances (Diva objects, LTO restore objects, etc)
 
 		# Duration takes presidence over end TC if both are supplied
 		if tc_duration is not None: self.tc_duration = tc_duration
 		elif tc_end is not None: self.tc_end = tc_end
 		else: raise ValueError("Either tc_duration or tc_end must be specified")
 
-		if metadata: self.add_metadata(metadata)
+		# Validate and add metadata
+		#if metadata: self.add_metadata(metadata)
 	
+	# Timecode properties
 	@property
 	def tc_start(self):
 		return self._tc_start	
@@ -327,14 +363,23 @@ class Shot:
 			self.tc_duration = tc_end - self.tc_start
 		else:
 			raise ValueError(f"TC End {tc_end} must not precede TC Start {self.tc_start}")
+	
+	# Instance properties
+	@property
+	def instances(self):
+		return self._instances
 
 	# TODO: Look in to making these @properties as well
-	def add_metadata(self, metadata):	
+	def addMetadata(self, metadata):	
 		# Remove special columns
 		self.metadata.update({key:val for key, val in metadata.items() if key not in self.__class__.SPECIAL_COLUMNS})
 	
-	def remove_metadata(self, metadata):
+	def removeMetadata(self, metadata):
 		if type(metadata) is dict:
 			{self.metadata.pop(key) for key, val in metadata.items() if self.metadata.get(key) == val}
 		else:
 			self.metadata.pop(metadata, None)
+	
+	# Media instances
+	def addInstance(self, instance):
+		self._instances.add(instance)
